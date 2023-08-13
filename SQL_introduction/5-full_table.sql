fde6ccc5ae907f80d771ad72f7b6e478a3f42771
Taste2 <stawiah17@gmail.com>
2023-08-13 13:34:56 +0000

    USE hbtn_test_db_5

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,12 +1,3 @@
 -- script that prints the full description of the table first_table from the database hbtn_0c_0
-DROP DATABASE IF EXISTS hbtn_0c_0;
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
-USE hbtn_0c_0;
-CREATE TABLE IF NOT EXISTS first_table (
-    id INT(11) NOT NULL AUTO_INCREMENT,
-    name VARCHAR(128),
-    c CHAR(1),
-    created_at DATE,
-    PRIMARY KEY (id)
-);
-SHOW CREATE TABLE hbtn_0c_0.first_table;
+
+SHOW CREATE TABLE hbtn_test_db_5.first_table;
