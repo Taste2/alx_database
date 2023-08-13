dd9280bc0f8e0bbea856232d7d58435257f388d5
Taste2 <stawiah17@gmail.com>
2023-08-13 10:23:01 +0000

    indexing db

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,5 +1,7 @@
 -- a script that creates a table called first_table in the current database in your MySQL server.
 
-USE mysql;
+USE mysql[1];
+
 DROP TABLE IF EXISTS first_table;
+
 CREATE TABLE first_table (id int, name varchar(256));
