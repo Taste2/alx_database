2adcf5c5f4af39dcf04c32e84682aaa03f170b6d
Taste2 <stawiah17@gmail.com>
2023-08-13 11:33:53 +0000

    changed db

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,7 +1,7 @@
 -- script that prints the full description of the table first_table from the database hbtn_0c_0
 
-USE hbtn_0c_0;
+USE hbtn_test_db_5;
 
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = hbtn_0c_0 AND TABLE_NAME = first_table;
+WHERE TABLE_SCHEMA = hbtn_test_db_5 AND TABLE_NAME = first_table;
