461fbb60d4b47fbb3057edbffc99f96e0a2a2e8a
Taste2 <stawiah17@gmail.com>
2023-08-13 12:39:00 +0000

    CREATE DB

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,5 @@
 -- script that prints the full description of the table first_table from the database hbtn_0c_0
-
+CREATE DATABASE hbtn_0c_0;
+USE hbtn_0c_0;
+CREATE TABLE first_table (id int, name varchar(256));
 SHOW CREATE TABLE hbtn_0c_0.first_table;
