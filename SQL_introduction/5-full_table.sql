e7119b85e8ddd8a24066e7540e680bcd6bc67b35
Taste2 <stawiah17@gmail.com>
2023-08-13 21:56:01 +0000

    fixing task5 error

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,4 @@
 -- script that prints the full description of the table first_table from the database hbtn_0c_0
 
-SHOW CREATE TABLE hbtn_test_db_5.first_table;
+USE hbtn_test_db_5
+SHOW CREATE TABLE first_table;
