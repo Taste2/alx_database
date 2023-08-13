1d01463fee9e2d40b911694cc08a7e93a3a96ddd
Taste2 <stawiah17@gmail.com>
2023-08-13 15:06:19 +0000

    REMOVED DROP STATEMENT

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,8 +1,8 @@
 -- Write a script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
 
 USE hbtn_test_db_9;
-DROP TABLE IF EXISTS second_table;
-CREATE TABLE second_table(id INT, name VARCHAR(256), score INT);
+
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
 
 INSERT INTO second_table
 VALUES(1, 'John', 10);
@@ -15,3 +15,4 @@ VALUES(3, 'Bob', 14);
 
 INSERT INTO second_table
 VALUES(4, 'George', 8);
+
