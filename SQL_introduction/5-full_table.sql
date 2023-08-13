9e8fa9eee2cfe45eb2a509dba173a7e6ab693506
Taste2 <stawiah17@gmail.com>
2023-08-13 11:40:21 +0000

    DIFFERENT METHOD

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,7 +1,3 @@
 -- script that prints the full description of the table first_table from the database hbtn_0c_0
 
-USE hbtn_test_db_5;
-
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = hbtn_test_db_5 AND TABLE_NAME = first_table;
+SHOW COLUMNS FROM hbtn_0c_0.first_table
