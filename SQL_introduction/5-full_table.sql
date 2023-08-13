012dafe4d1925297581d64c03953983ffebaf68a
Taste2 <stawiah17@gmail.com>
2023-08-13 10:42:16 +0000

    CREATE TABLE

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,5 @@
 -- script that prints the full description of the table first_table from the database hbtn_0c_0
 
 USE hbtn_0c_0;
-
+CREATE TABLE first_table (id int, name varchar(256));
 SHOW TABLE first_table;
