2a294f5f32d17938f4d11880d4a69f9e1f707cee
Taste2 <stawiah17@gmail.com>
2023-08-13 11:58:42 +0000

    NEW TABLE

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -4,6 +4,10 @@ CREATE DATABASE hbtn_0c_0;
 
 USE hbtn_0c_0;
 
-CREATE TABLE first_table (id int, name varchar(256));
+CREATE TABLE first_table (id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(128),
+    c CHAR(1),
+    created_at DATE,
+    PRIMARY KEY (id));
 
 SHOW COLUMNS FROM first_table;
