aecc1209f11be33cfb3a0ec2609c7a7f4a2ab81a
Taste2 <stawiah17@gmail.com>
2023-08-13 11:16:23 +0000

    USE INFORMATON SCHEMA

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,5 @@
 -- script that prints the full description of the table first_table from the database hbtn_0c_0
 
-USE hbtn_0c_0;
-CREATE TABLE first_table (id int, name varchar(256));
-SHOW TABLE first_table;
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = hbtn_0c_0 AND TABLE_NAME = first_table;
