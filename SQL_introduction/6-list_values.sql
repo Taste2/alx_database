e25953bbb7dd63a3964682f18e05fe1fc4acd422
Taste2 <stawiah17@gmail.com>
2023-08-13 12:52:44 +0000

    Change database

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,4 +1,4 @@
 --  script that lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server.
 
-USE hbtn_0c_0;
-SHOW VALUES first_table;
+USE hbtn_test_db_6;
+SHOW TABLE first_table;
