5897274f3ddb13a4e75b773a133d93344f10a9e2
Taste2 <stawiah17@gmail.com>
2023-08-13 12:42:33 +0000

    DROP AND CREATE DB AND TABLE

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,12 @@
 -- script that prints the full description of the table first_table from the database hbtn_0c_0
-CREATE DATABASE hbtn_0c_0;
+DROP DATABASE IF EXISTS hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 USE hbtn_0c_0;
-CREATE TABLE first_table (id int, name varchar(256));
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(128),
+    c CHAR(1),
+    created_at DATE,
+    PRIMARY KEY (id)
+);
 SHOW CREATE TABLE hbtn_0c_0.first_table;
