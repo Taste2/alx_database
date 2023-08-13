887d0bf0e98633bdf63af406b1909ed301080a80
Taste2 <stawiah17@gmail.com>
2023-08-13 12:28:58 +0000

    revert db

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- script that prints the full description of the table first_table from the database hbtn_0c_0
 
-SHOW CREATE TABLE hbtn_test_db_5.first_table;
+SHOW CREATE TABLE hbtn_0c_0.first_table;
