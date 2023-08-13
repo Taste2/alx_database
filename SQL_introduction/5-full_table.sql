6531cec23e58c663d40f88c3440e588877e65cd0
Taste2 <stawiah17@gmail.com>
2023-08-13 12:11:42 +0000

    removed all creations

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,13 +1,3 @@
 -- script that prints the full description of the table first_table from the database hbtn_0c_0
 
-CREATE DATABASE hbtn_0c_0;
-
-USE hbtn_0c_0;
-
-CREATE TABLE first_table (id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(128),
-    c CHAR(1),
-    created_at DATE,
-    PRIMARY KEY (id));
-
 SHOW CREATE TABLE hbtn_0c_0.first_table;
