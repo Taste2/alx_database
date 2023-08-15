0b2b6c2d751f19c082141dae4077e0c362723d7e
Taste2 <stawiah17@gmail.com>
2023-08-15 18:27:47 +0000

    lowercase of attr NAME

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,3 +1,3 @@
 -- Write a script that creates the table force_name
 
-CREATE TABLE IF NOT EXISTS force_name(id INT, NAME VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS force_name(id INT, name VARCHAR(256) NOT NULL);
